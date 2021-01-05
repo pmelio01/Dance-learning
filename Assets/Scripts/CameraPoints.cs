66dcbae517642ec5e910d068822ef584946f1fd7
pmelio01 <pmelio01@cs.ucy.ac.cy>
2021-01-05 12:26:41 +0200

    House and Intro
    House and intro animation created.

---
+++ Assets/Scripts/CameraPoints.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPoints : MonoBehaviour {
+
+	void CameraPoint1()
+    {
+        Debug.Log("Camera is not at point 1");
+    }
+}
