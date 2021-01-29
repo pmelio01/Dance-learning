ac9ed14834103c8885b02bf1e339c3493be59c57
pmelio01 <pmelio01@cs.ucy.ac.cy>
2021-01-29 20:04:05 +0200

    All cameras are now done.

---
+++ Assets/Scripts/IntroScene/Data/AuthManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AuthManager : MonoBehaviour
+{
+    [Header("Camera for clip selection")]
+    public GameObject Vcam2;
+
+    //Firebase variables
+    //[Header("Firebase")]
+    //public DependencyStatus dependencyStatus;
+    //public FirebaseAuth auth;
+    //public FirebaseUser User;
+
+    //Login variables
+    //[Header("Login")]
+    //public TMP_InputField loginEmail;
+    //public TMP_InputField loginPassword;
+
+    //[Header("Register")]
+    // public TMP_InputField registerName;
+    //public TMP_InputField registerEmail;
+    //public TMP_InputField registerPassword;
+    //public TMP_InputField registerConfirmPassword;
+
+
+    private void Awake()
+    {
+        
+    }
+
+
+    public void LoginButton()
+    {
+       
+        Vcam2.SetActive(true);
+    }
+
+
+    public void RegisterButton()
+    {
+        //MUST FIX IT
+        //StartCoroutine(Register(registerName.text, registerEmail.text, registerPassword.text));
+        //UIManager.instance.LoginScreen();
+    }
+
+}
---
+++ Assets/Scripts/IntroScene/GamePlay/GamePlay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlay : MonoBehaviour
+{
+    [Header("Camera for Gameplay")]
+    public GameObject Vcam3;
+
+    public void Awake()
+    {
+        
+    }
+
+    public void PlayDanceButton()
+    {
+        Vcam3.SetActive(true);
+    }
+}
